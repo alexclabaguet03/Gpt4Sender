be6b812742c1379a4199d0477c04eeaf588181b4
alexclabaguet03 <109463853+alexclabaguet03@users.noreply.github.com>
2023-03-30 01:03:49 +0200

    Update Gpt4Sender_test.py

--- Gpt4Sender_test.py
+++ Gpt4Sender_test.py
@@ -8,9 +8,10 @@ Created on Wed Mar 29 22:33:14 2023
 import Gpt4Sender 
 
 API_KEY = "your_api_key"
-test = Gpt4Sender("testingthisout@outlook.fr","Cacahuete(-)",
-                  API_KEY,
-                  "https://api.openai.com/v1/chat/completions",1,0)
+sender_email = "your_email"
+account_password = "email_account_password"
+
+test = Gpt4Sender(sender_email,account_password,API_KEY,"https://api.openai.com/v1/chat/completions",1,0)
 
 
 test.send_email()
